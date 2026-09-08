64f9012d078e38b4a4f9d9c3d179def733646c34
salmannawaz <salman.nawaz@arbisoft.com>
2026-09-08 15:37:30 +0500

    fix: review comments

--- openassessment/__init__.py
+++ openassessment/__init__.py
@@ -2,9 +2,5 @@
 Initialization Information for Open Assessment Module
 """
 
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("ora2")
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
+from importlib.metadata import version
+__version__ = version("ora2")
